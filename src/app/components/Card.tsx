4ec502fe5fcfef0ea1fbade8b85d158034d450e6
Vikram_761 <srimanvgn@gmail.com>
2023-09-06 14:38:36 +0530

    completed the rest of the functiionality

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -4,25 +4,34 @@ interface CardProps {
   city: any;
   cities: any;
   setCities: any;
+  isFaren: boolean;
 }
 import { BsFillTrash3Fill } from "react-icons/bs";
 
-const Card: FC<CardProps> = ({ city, cities, setCities }) => {
-  const temp = Math.round(city.main.temp - 273.15);
+const Card: FC<CardProps> = ({ city, cities, setCities, isFaren }) => {
+  const tempc = Math.round(city.main.temp - 273.15);
+  const tempf = Math.round(tempc * 1.8 + 32);
+  const icon = city.weather[0].icon;
 
   const deleteCity = (city: any) => {
     setCities(cities.filter((c: any) => c.name !== city.name));
   };
+  console.log(city.weather);
   return (
-    <div className=" w-1/3 max-w-md max-md:w-full bg-white/50  px-4 py-6">
+    <div className=" w-1/3 max-w-md max-md:w-full bg-white/50  px-6 py-6 rounded-lg shadow-xl">
+      <div className="w-full flex justify-center ">
+        <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
+      </div>
       <div className="flex">
         <div className=" font-medium flex flex-col w-[90%] gap-4 py-4">
-          <h2 className="sm:text-xl text-lg">CITY : {city.name}</h2>
-          <h2 className="sm:text-xl text-lg">TEMPERATURE : {temp} C</h2>
+          <h2 className="sm:text-xl text-lg">City : {city.name}</h2>
+          <h2 className="sm:text-xl text-lg">
+            Temperature : {isFaren ? `${tempf} F` : `${tempc} C`}
+          </h2>
         </div>
         <div className="mt-4">
           <BsFillTrash3Fill
-            className="text-2xl "
+            className="text-2xl text-gray-600 transition-all hover:-translate-y-1 duration-100 ease-in-out "
             onClick={() => deleteCity(city)}
           />
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,22 @@ export default function Home() {
         alert("city is not found");
       } else {
         setCities([weather, ...cities]);
+        setSearch("");
       }
     } catch (err) {
       console.log(err);
     }
   };
+  const [isToggled, setToggled] = useState(false);
+  const toggle = () => {
+    setToggled(!isToggled);
+  };
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen w-full relative">
-      <h1 className="md:text-6xl text-4xl text-center text-white pt-20 pb-16 font-bold px-4">
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen w-full relative py-20">
+      <h1 className="md:text-6xl text-4xl text-center text-white  pb-16 font-bold px-4">
         Weather Application
       </h1>
+
       <div className="flex w-full justify-center px-4 gap-4">
         <Input
           value={search}
@@ -35,17 +41,40 @@ export default function Home() {
           placeholder="Enter your city"
         />
         <button
-          className="py-2 rounded-lg bg-black text-white text-md border-0 transition-all hover:-translate-y-1 duration-150 px-6 "
+          className="hover:bg-gray-700 py-2 rounded-lg bg-black text-white text-md border-0 transition-all hover:-translate-y-1 duration-150 px-6 ease-in-out"
           onClick={addCity}
         >
           Add
         </button>
       </div>
 
+      <div className="text-2xl flex gap-2 font-bold justify-center mt-6 text-white">
+        <span>C</span>
+        <button
+          className={`w-16 h-8 bg-gray-300 rounded-full p-1 duration-300 ease-in-out  ${
+            isToggled ? "bg-blue-500" : ""
+          }`}
+          onClick={toggle}
+        >
+          <div
+            className={`bg-white w-5 h-5 rounded-full shadow-md transform duration-300 ease-in-out ${
+              isToggled ? "translate-x-8 " : ""
+            }`}
+          ></div>
+        </button>
+        <span>F</span>
+      </div>
+
       <div className="w-full flex max-md:flex-col max-md:justify-center max-md:items-center max-md:gap-8 mt-20 justify-center gap-4 px-6">
         {cities.map((c: any) => {
           return (
-            <Card key={c.name} city={c} cities={cities} setCities={setCities} />
+            <Card
+              key={c.name}
+              city={c}
+              cities={cities}
+              setCities={setCities}
+              isFaren={isToggled}
+            />
           );
         })}
       </div>
